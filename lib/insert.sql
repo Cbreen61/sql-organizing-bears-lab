7af37ef3f198045d420527f31f2cead974032b3a
Christopher Breen <C.jbreen61@gmail.com>
2020-12-07 15:17:09 -0500

    commit tree 6aa392a6474686eef089ed2df339f753e4d6af80

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color) VALUES ('MR. Chocolate', 3, 'M', 'Brown');
+INSERT INTO bears (name, age, sex, color) VALUES ('Rowdy' , 4, 'F', 'green');
+INSERT INTO bears (name, age, sex, color) VALUES ('Tabitha', 3, 'M', 'Brown');
+INSERT INTO bears (name, age, sex, color) VALUES ('Sergeant Brown', 8, 'M', 'Brown');
+INSERT INTO bears (name, age, sex, color) VALUES ('Melissa', 7, 'M', 'black');
+INSERT INTO bears (name, age, sex, color) VALUES ('Grinch', 6, 'M', 'Brown');
+INSERT INTO bears (name, age, sex, color) VALUES ('Wendy', 4, 'M', 'white');
+INSERT INTO bears (name, age, sex, color) VALUES (Null, 10, 'M', 'Black' );
